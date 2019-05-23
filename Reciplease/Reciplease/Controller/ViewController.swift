c12f379a7eb32c21fca4d7134eee4c1b71143aee
kcourtois <courtois.k2@gmail.com>
2019-05-23 15:17:14 +0200

    Created base project, setup SwiftLint and travis.yml config file

---
+++ Reciplease/Reciplease/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Kévin Courtois on 23/05/2019.
+//  Copyright © 2019 Kévin Courtois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
